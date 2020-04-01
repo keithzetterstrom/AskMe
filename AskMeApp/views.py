02903345319309ad478193fd0226ad69b8e181c2
keithzetterstrom <mariyapmx@mail.ru>
2020-04-01 15:08:10 +0300

    Добавлены html файлы основных страниц проекта, настроена навигация сайта и шаблонизация.

--- AskMeApp/views.py
+++ AskMeApp/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
-questions = {
-    i: {'id': i, 'title': f'question # {i}'}
-    for i in range(5)
-}
+# questions_dct = {
+#     i: {'id': i, 'title': f'question # {i}', 'text': f'text ))) {i}'}
+#     for i in range(5)
+# }
 
+questions_dct = [
+    {'id': 0, 'title': f'question # 0', 'text': f'text ))) 0', 'like': 1, 'data': '22.02.2020'},
+    {'id': 1, 'title': f'question # 1', 'text': f'text ))) 1', 'like': 2, 'data': '22.02.2020'},
+    {'id': 2, 'title': f'question # 2', 'text': f'text ))) 2', 'like': 4, 'data': '22.02.2020'},
+    {'id': 3, 'title': f'question # 3', 'text': f'text ))) 3', 'like': 7, 'data': '22.02.2020'},
+    {'id': 4, 'title': f'question # 4', 'text': f'text ))) 4', 'like': 11, 'data': '22.02.2020'},
+    {'id': 5, 'title': f'question # 5', 'text': f'text ))) 5', 'like': 0, 'data': '22.02.2020'},
+]
 
 def index(request):
     return HttpResponse('hello')
@@ -23,3 +32,38 @@ def unauthorisation(request):
 
 def login(request):
     return render(request, 'login.html')
+
+
+def sing_up(request):
+    return render(request, 'signup.html')
+
+
+def settings(request):
+    return render(request, 'settings.html')
+
+
+def ask(request):
+    return render(request, 'ask.html')
+
+
+def questions(request):
+    paginator = Paginator(questions_dct, 3)  # 3 поста на каждой странице
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом, поставим первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если страница больше максимальной, доставить последнюю страницу результатов
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'questions.html', {'page': page, 'dct': posts})
+
+
+def question(request, question_id):
+
+    return render(request, 'question.html', {'dct': questions_dct[int(question_id)]})
+
+
+def hot(request):
+    return render(request, 'questions.html')
